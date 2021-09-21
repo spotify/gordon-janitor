b478f8d0b6f9008bbc8b6d8d437f8418e9155b69
Anurag Kumar <mailanu98@gmail.com>
2021-09-21 19:49:34 +0530

    Update setup.py
    updated classifiers with python versions `3.7 , 3.8 , 3.9`

--- setup.py
+++ setup.py
@@ -76,6 +76,9 @@ CLASSIFIERS = [
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: Implementation :: CPython',
     'Topic :: Internet :: Name Service (DNS)',
 ]
